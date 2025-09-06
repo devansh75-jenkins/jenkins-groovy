d413a9ad43e34949308d4aec81c4b95aa2566cda
devansh75-jenkins <suryakantlodhi75@gmail.com>
2025-09-06 22:05:18 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -68,14 +68,6 @@ def call(Map args = [:]) {
     // IMAGE_TAG ko environment variable bana do taaki agli stages use kar saken
     env.IMAGE_TAG = tag
     return tag
-  if (!args.image) error "image name required"
-  if (!args.dockerfile) args.dockerfile = 'Dockerfile'
-  // immutable tag: commit sha + build number
-  def sha = sh(script: "git rev-parse --short HEAD", returnStdout: true).trim()
-  def tag = "${args.image}:${sha}-${env.BUILD_NUMBER}"
-  sh "docker build -f ${args.dockerfile} -t ${tag} ."
-  env.IMAGE_TAG = tag          // make global & visible in Jenkins UI
-  return tag
 }
 
 
