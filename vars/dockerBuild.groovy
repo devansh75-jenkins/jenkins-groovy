ea0a6fce2de3c611832934f9989f556dfa2ea7cf
devansh75-jenkins <suryakantlodhi75@gmail.com>
2025-09-06 18:58:45 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,3 +1,21 @@
+// # Dependencies
+// sudo apt-get update
+// sudo apt-get install -y ca-certificates curl gnupg lsb-release
+
+// # Docker GPG Key
+// sudo mkdir -p /etc/apt/keyrings
+// curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+// # Docker Repo Add
+// echo \
+//   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+//   https://download.docker.com/linux/ubuntu \
+//   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+// # Install Docker + Buildx + Compose
+// sudo apt-get update
+// sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 // vars/buildImage.groovy
 // -------------------------------------------------------
 // 📌 Function: buildImage
